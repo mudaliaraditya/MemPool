17410791abf7805af4d5cb44034b7b31b66b64bd
Aditya Mudaliar <mudaliaraditya@gmail.com>
2021-07-03 18:46:56 +0530

    changes for operator new and delete

---
+++ MemPool/NewChunk.h
@@ -0,0 +1,73 @@
+#ifndef NEW_DEFINES
+#define NEW_DEFINES
+#include "PoolManager.h"
+#include<new>
+void* operator new(std::size_t siz) throw()
+{
+	CPoolManager& p = CPoolManager::GetInst();
+	void* pAddr = p.GetChunk(siz);
+	return pAddr;
+}
+
+void* operator new(std::size_t siz,const std::nothrow_t &tag) noexcept
+{
+	try
+	{
+		CPoolManager& p = CPoolManager::GetInst();
+		void* pAddr = p.GetChunk(siz);
+		return pAddr;
+	}
+	catch(std::exception& e)
+	{
+		return nullptr;
+	}
+	catch(...)
+	{
+		return nullptr;
+	}
+}
+
+void* operator new[](std::size_t siz) throw()
+{
+	CPoolManager& p = CPoolManager::GetInst();
+	void* pAddr = p.GetChunk(siz);
+	return pAddr;
+}
+
+void* operator new[](std::size_t siz,const std::nothrow_t &tag) noexcept
+{
+	try
+	{
+		CPoolManager& p = CPoolManager::GetInst();
+		void* pAddr = p.GetChunk(siz);
+		return pAddr;
+	}
+	catch(std::exception& e)
+	{
+		return nullptr;
+	}
+	catch(...)
+	{
+		return nullptr;
+	}
+}
+
+void operator delete(void* pVoid)
+{
+	CPoolManager::GetInst().ReleaseChunk(pVoid);
+	return;
+}
+
+void operator delete[](void* pVoid)
+{
+	CPoolManager::GetInst().ReleaseChunk(pVoid);
+	return;
+}
+
+
+
+
+
+
+
+#endif
--- example2/test3.cpp
+++ example2/test3.cpp
@@ -1,10 +1,26 @@
-#include "newdefines.h"
-#include "PoolManager.h"
-#include "MemoryPool.h"
+#include "NewChunk.h"
+#include <iostream>
 
+class A
+{
+	public:
+		A()
+		{
+		}
+
+		~A()
+		{
+			std::cout << "dtor for A" << std::endl;
+		}
+
+
+};
 
 int main(int argc,char** argv)
 {
 	int* p = new int();
+	A* a = new A();
+	delete a;
+	delete p;
 }
 
